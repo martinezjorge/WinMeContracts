d7a22a5eafa0087d37a195f9d52503450ac87b90
Jorge Martinez <martinezjorge273@gmail.com>
2024-02-23 21:00:42 -0700

    Adding ownable to WinMeTokenClaim contract

--- src/WinMeTokenClaim.sol
+++ src/WinMeTokenClaim.sol
@@ -1,16 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-contract WinMeTokenClaim {
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract WinMeTokenClaim is Ownable {
     uint256 public number;
 
     mapping(address => bool) public approvedTokens;
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+    event TokenApproved(address indexed token, bool indexed approvedStatus);
+
+    constructor() Ownable(msg.sender) {}
+
+    /// @notice Allows the contract owner to toggle whether an address is a valid payment token for escrow
+    /// @param token The address of the token to add or remove
+    function togglePaymentToken(address token) external onlyOwner {
+        approvedTokens[token] = !approvedTokens[token];
+        emit TokenApproved(token, approvedTokens[token]);
     }
 
-    function increment() public {
-        number++;
-    }
 }
