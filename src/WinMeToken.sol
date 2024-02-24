aa9fdbeb96874a937fc9cb413796429f039a9d69
Jorge Martinez <martinezjorge273@gmail.com>
2024-02-23 20:56:50 -0700

    made win me token ownable

--- src/WinMeToken.sol
+++ src/WinMeToken.sol
@@ -2,9 +2,10 @@
 pragma solidity 0.8.20;
 
 import "@solmate/tokens/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MockERC20 is ERC20 {
-    constructor() ERC20("Test", "TT", 18) {}
+contract WinMeToken is ERC20, Ownable {
+    constructor() ERC20("Test", "TT", 18) Ownable(msg.sender) {}
 
     function mint(address to, uint256 amount) public {
         _mint(to, amount);
