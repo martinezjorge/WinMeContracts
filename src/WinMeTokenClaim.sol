49357c87642682005393b8e36021582bd2747c22
Jorge Martinez <martinezjorge273@gmail.com>
2024-02-24 17:59:25 -0700

    Changed IERC20 to IERC20Permit

--- src/WinMeTokenClaim.sol
+++ src/WinMeTokenClaim.sol
@@ -2,12 +2,12 @@
 pragma solidity 0.8.20;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Permit.sol";
 
 contract WinMeTokenClaim is Ownable {
     uint256 public number;
-    using SafeERC20 for IERC20;
+    using SafeERC20 for IERC20Permit;
     mapping(address => bool) public approvedTokens;
 
     event TokenApproved(address indexed token, bool indexed approvedStatus);
