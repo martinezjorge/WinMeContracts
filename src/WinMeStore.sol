ace3064b81e3f1b55a0d2821ccaa94fd98c8fb02
Jorge Martinez <martinezjorge273@gmail.com>
2024-03-01 08:31:51 -0700

    WIP, committing to install chainlink contracts

--- src/WinMeStore.sol
+++ src/WinMeStore.sol
@@ -4,37 +4,51 @@ pragma solidity 0.8.20;
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+// import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 /// @notice You should always use SafeERC20 to transfer ERC20 tokens
 contract WinMeStore is Ownable {
     using SafeERC20 for IERC20;
 
-    address usdcTokenAddress;
-    address usdtTokenAddress;
-    address openDollarTokenAddress;
+    IERC20 winMeToken;
+    address treasury;
+    uint256 networkCurrencyPrice;
 
-    constructor() Ownable(msg.sender) {}
+    struct TokenRegistry {
+        uint256 decimals;
+        bool active;
+    }
+
+    mapping(address => TokenRegistry) paymentTokens;
+
+    constructor(address _winMeTokenAddress, address _treasuryAddress) Ownable(msg.sender) {
+        winMeToken = IERC20(_winMeTokenAddress);
+        treasury = _treasuryAddress;
+    }
 
     /// @notice a function to purchase WinMeToken with ETH
     /// @notice watch out for reentrancy attacks
     /// @notice on arbitrum this will be ARB, on optimism OP, on Polygon MATIC
-    function purchaseWithNetworkCurrency() external payable {}
+    function purchaseWinMeTokenWithNetworkCurrency() external payable {
+        require(msg.value == networkCurrencyPrice);
+    }
 
     /// @notice should allow users to purchase WinMe token with USDT
-    function purchaseWithUSDT() external {}
+    function purchaseWinMeTokenWithERC20(address _tokenAddress) external {
 
-    /// @notice should allow users to purchase WinMe token with USDC
-    function purchaseWithUSDC() external {}
+    }
 
-    function purchaseWithOpenDollar() external {}
+    function purchaseWinMeNftWithNetworkCurrency() external payable {}
 
-    /// @notice should allow the owner to transfer the eth out of the contract
-    function retrieveNetworkCurrencyInContract() external onlyOwner {}
+    function purchaseWinMeNftWithERC20() external {}
 
-    /// setter functions for the stablecoins
-    function setUSDCAddress(address _usdcAddress) external onlyOwner {}
+    // ADMIN FUNCTIONS
+    function setTreasuryAddress(address _newTreasuryAddress) external onlyOwner {
+        treasury = _newTreasuryAddress;
+    }
 
-    function setUSDTAddress(address _usdcAddress) external onlyOwner {}
-
-    function setOpenDollarAddress(address _usdcAddress) external onlyOwner {}
+    function setNetworkCurrencyPrice(uint256 _newPrice) external onlyOwner {
+        networkCurrencyPrice = _newPrice;
+    }
 }
