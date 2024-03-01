695b2ea5b94cdbbe847f5d5a864c1e19380aa988
Jorge Martinez <martinezjorge273@gmail.com>
2024-03-01 12:26:27 -0700

    added tests checking pricefeed works

---
+++ test/WinMeStore - purchaseWinMeTokenWithNetworkCurrency.js
@@ -0,0 +1,40 @@
+const {
+    loadFixture,
+    impersonateAccount,
+    setBalance,
+  } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+  require("@nomicfoundation/hardhat-ethers");
+  const { expect } = require("chai");
+  const { ethers } = require("hardhat");
+  const moment = require("moment");
+  const { deployTokenFixture } = require("../common/test-common");
+  
+  describe("GeoJamEscrow-newEscrowViaBuyer", function () {
+    describe("Approved Transactions", function () {
+      it("WinMeStore should have a link balance", async function () {
+        const {
+          token,
+          store,
+          link,
+          others: { treasury, addr3 },
+        } = await loadFixture(deployTokenFixture);
+        const storeBalance = await link.balanceOf(store.target);
+        let tokenAmount = await store.winMeTokenAmount(ethers.parseEther("1"));
+      });
+
+      it("Should let a user purchase WinMeTokens using Network Currency", async function () {
+        const {
+          token,
+          store,
+          link,
+          others: { treasury, addr3 },
+        } = await loadFixture(deployTokenFixture);
+        await store.connect(addr3).purchaseWinMeTokenWithNetworkCurrency({value: ethers.parseEther("1")});
+        const userWinMeBalance = await token.balanceOf(addr3.address);
+        console.log(ethers.formatEther(userWinMeBalance));
+      });
+    });
+  
+    // describe("Reverted Transactions", function () {});
+  });
+  
