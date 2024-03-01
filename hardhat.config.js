523252363bafb27a0f21a4799c3e786c905f93ad
Jorge Martinez <martinezjorge273@gmail.com>
2024-03-01 10:32:47 -0700

    setting up hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,5 +3,22 @@ require("@nomicfoundation/hardhat-foundry");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+  solidity: "0.8.20",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      forking: {
+        url: `https://mainnet.infura.io/v3/${CONFIG[0]}`,
+        blockNumber: 19267162,
+      }
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${CONFIG[0]}`,
+      accounts: [CONFIG[2]],
+    },
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${CONFIG[1]}`,
+      accounts: [CONFIG[3]],
+    },
+  },
 };
