e1736c218251032c842a7d0ffbfff00118bb7413
Jorge Martinez <martinezjorge273@gmail.com>
2024-03-02 02:11:07 -0700

    updated interface

--- src/interfaces/IWinMeToken.sol
+++ src/interfaces/IWinMeToken.sol
@@ -2,5 +2,9 @@
 pragma solidity 0.8.20;
 
 interface IWinMeToken {
+    function transfer(address to, uint256 value) external returns (bool);
+
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
+
     function mint(address account, uint256 amount) external;
 }
